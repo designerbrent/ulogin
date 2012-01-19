6546d6bc7e5ef5a852347fbf77484a40031f4838
James Jeffery <jaysquare@gmail.com>
2012-01-19 11:15:11 -0800

    Wrapped behavior code within a processed check.

--- ulogin.js
+++ ulogin.js
@@ -6,35 +6,38 @@
 $('<style type="text/css">.ulogin #register-form, .ulogin #login-form {display:none;}</style>').appendTo('head');
 
 Drupal.behaviors.ulogin = function (context) {
-  // Remove potentially confusing active classes on "tabs"
-  $('.ulogin a.active').removeClass('active');
-    
-  // Attach behaviors to the links so that they show/hide forms appropriately.
-  $('.ulogin #register-link').click(function() {
-    $(this).addClass('ul-active').blur();
-    $('.ulogin #login-link').removeClass('ul-active');
-    $('.ulogin #register-form').show();
-    $('.ulogin #login-form').hide();
-    return false;
-  });
-  $('.ulogin #login-link').click(function() {
-    $(this).addClass('ul-active').blur();
-    $('.ulogin #register-link').removeClass('ul-active');
-    $('.ulogin #login-form').show();
-    $('.ulogin #register-form').hide();
-    return false;
-  });
-  
-  switch(Drupal.settings.ulogin.activeForm) {
-    case 'register':
+  if ($('.ulogin:not(.ulogin-processed)').size()) {
+    // Remove potentially confusing active classes on "tabs"
+    $('.ulogin a.active').removeClass('active');
+
+    // Attach behaviors to the links so that they show/hide forms appropriately.
+    $('.ulogin #register-link').click(function() {
+      $(this).addClass('ul-active').blur();
+      $('.ulogin #login-link').removeClass('ul-active');
       $('.ulogin #register-form').show();
-      $('.ulogin #register-link').click();
-      break;
-    case 'login':
-    default:
+      $('.ulogin #login-form').hide();
+      return false;
+    });
+    $('.ulogin #login-link').click(function() {
+      $(this).addClass('ul-active').blur();
+      $('.ulogin #register-link').removeClass('ul-active');
       $('.ulogin #login-form').show();
-      $('.ulogin #login-link').click();
-      break;
+      $('.ulogin #register-form').hide();
+      return false;
+    });
+
+    switch(Drupal.settings.ulogin.activeForm) {
+      case 'register':
+        $('.ulogin #register-form').show();
+        $('.ulogin #register-link').click();
+        break;
+      case 'login':
+      default:
+        $('.ulogin #login-form').show();
+        $('.ulogin #login-link').click();
+        break;
+    }
+
+    $('.ulogin').addClass('ulogin-processed');
   }
-    
 }
